3cff21710a055760411cd8cf4e071b6649ed2ca6
Brandon O <b.ondayko@gmail.com>
2020-10-21 13:16:18 -0500

    added the question variables

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+var wrapperEl = document.querySelector("#questions-section");
+
+//Question variables
+var question1 = {
+    title: "What is a variable?",
+    firstC: "A place to store numbers",
+    seconedC: "A place to store strings",
+    thirdC: "A Javascript Element",
+    fourthC: "All of the above",
+};
+
+var question2 = {
+    title: "What is a string?",
+    firstC: "A CSS property",
+    seconedC: "A numerical value",
+    thirdC: "Text",
+    fourthC: "A type of function",
+};
+
+var question3 = {
+    title: "Which of the following is a boolean value?",
+    firstC: "3",
+    seconedC: "yes",
+    thirdC: "undefined",
+    fourthC: "true",
+};
+
+var question4 = {
+    title: "Which of the following is an img file type?",
+    firstC: ".png",
+    seconedC: ".ref",
+    thirdC: ".href",
+    fourthC: ".css",
+};
+
+var question5 = {
+    title: "What is the internet?",
+    firstC: "Cats?",
+    seconedC: "Cats?",
+    thirdC: "Cats?",
+    fourthC: "Cats?",
+};
+
